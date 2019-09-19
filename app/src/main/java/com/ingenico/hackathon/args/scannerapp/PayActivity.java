8a3b80bae9067ddf7935f44cde9c318c073cfe63
devajani <gdevajani@gmail.com>
2019-09-19 17:16:03 +0200

    ogone payment page showing

--- app/src/main/java/com/ingenico/hackathon/args/scannerapp/PayActivity.java
+++ app/src/main/java/com/ingenico/hackathon/args/scannerapp/PayActivity.java
@@ -3,6 +3,10 @@ package com.ingenico.hackathon.args.scannerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
 
 public class PayActivity extends AppCompatActivity {
 
@@ -11,7 +15,18 @@ public class PayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
 
-        System.out.println("Am INNNNNs");
+
+        String url = "https://secure.ogone.com/Ncol/Test/orderstandard_UTF8.asp?AMOUNT=1000&BRAND=VISA&CURRENCY=EUR&LANGUAGE=en_US&OPERATION=SAL&ORDERID=TestHackathon1&PM=Creditcard&PSPID=ADATest&SHASIGN=B817C7CB4783D4EB5B2543091023FB1DD054798A";
+        WebView view = (WebView) findViewById(R.id.myWebView);
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
     }
 
 
