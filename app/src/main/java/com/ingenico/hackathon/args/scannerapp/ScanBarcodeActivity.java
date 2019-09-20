cdcf7ce96ec680966b2f4e897ca5f323ef58f1ce
Marcos Mestre <marcos.mestre@epay.ingenico.com>
2019-09-20 09:09:13 +0200

    Formatted as two decimals. Added another tab between the text and the values.

--- app/src/main/java/com/ingenico/hackathon/args/scannerapp/ScanBarcodeActivity.java
+++ app/src/main/java/com/ingenico/hackathon/args/scannerapp/ScanBarcodeActivity.java
@@ -55,9 +55,9 @@ public class ScanBarcodeActivity extends AppCompatActivity implements ZXingScann
         for ( String currentResult: finalList) {
             Float currentAmount = priceMap.get(currentResult);
             amount += currentAmount;
-            DisplayMessage += String.format("%s:\t%s\n", currentResult, currentAmount);
+            DisplayMessage += String.format("%s:\t\t%.2f\n", currentResult, currentAmount);
         }
-        DisplayMessage += "Total: " + amount;
+        DisplayMessage += String.format("Total:\t\t%.2f", amount);
         MainActivity.resulTextView.setText(DisplayMessage);
 
 
